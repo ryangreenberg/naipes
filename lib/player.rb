06088917f888b2440b4f9fc9d31c5bf084975868
Ryan Greenberg <ryangreenberg@gmail.com>
2011-04-05 07:41:19 -0700

    Added take_turn to Player

--- lib/player.rb
+++ lib/player.rb
@@ -4,8 +4,13 @@ class Player
     @name = name
     @cards = []
   end
-  
+
   def draw(deck)
     @cards << deck.draw
+  end
+
+  def take_turn(game)
+    puts "#{@name}'s turn..."
+    game.allowed?(:action => :draw)
   end
 end
